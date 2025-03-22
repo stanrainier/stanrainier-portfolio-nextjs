c10512e6e4c562b391a4f64ce62df6edf86d7084
stanrainier <stanrainiersalazar@gmail.com>
2025-03-22 22:42:33 +0800

    initial commit

---
+++ config/site.ts
@@ -0,0 +1,43 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "Dev Stan | Portfolio",
+  description: "A creative portfolio developed by Stan Rainier D. Salazar",
+  navItems: [
+    {
+      label: "Projects",
+      href: "/projects",
+    },
+    {
+      label: "About",
+      href: "/about",
+    },
+    {
+      label: "Contact",
+      href: "/contact",
+    },
+  ],
+  navMenuItems: [
+    {
+      label: "Home",
+      href: "/",
+    },
+    {
+      label: "Projects",
+      href: "/projects",
+    },
+    {
+      label: "About",
+      href: "/about",
+    },
+    {
+      label: "Contact",
+      href: "/contact",
+    },
+   
+  ],
+  links: {
+    github: "https://github.com/stanrainier",
+    buymecoffe: "https://buymeacoffee.com/stanrainiersalazar",
+  },
+};
