76783e2be7b1437db209fb80c9e450a34ed670c4
stanrainier <stanrainiersalazar@gmail.com>
2025-04-02 01:42:32 +0800

    added alert

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { Snippet } from "@heroui/snippet";
 import { Code } from "@heroui/code";
 import { siteConfig } from "@/config/site";
 import { GithubIcon, GitlabIcon } from "@/components/icons";
-import { Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Button, Divider } from "@heroui/react";
+import { Alert } from "@heroui/react";
 import { motion } from "motion/react"
 import Contact from "@/components/contact";
 import About from "@/components/about";
@@ -12,9 +12,12 @@ import Projects from "@/components/projects";
 import Image from "next/image";
 import Hero from "@/components/hero";
 
+
 export default function Home() {
   return (
     <div>
+      <Alert description="This website is still under construction" title="Under Construction" color="secondary" variant="bordered" />
+
 
     <motion.div initial={{ scale: 0 }} animate={{ scale: 1 }} exit={{ scale: 0 }}>
       <Hero />
