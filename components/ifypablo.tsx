bd12838007eda3c59d552d7df0702bdf7ea36c49
Stan <stanrainier.salazar.po@gmail.com>
2026-05-09 18:34:33 +0800

    fix

--- components/ifypablo.tsx
+++ components/ifypablo.tsx
@@ -89,7 +89,7 @@ export default function IfypabloComponent() {
                 scrolling="no" 
                 src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/2047237104&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true" 
                 title="SoundCloud Player: ifypablo x slate - di ko pa ma amin ft. jdel" 
-                width="100%">
+                width="100%"
               ></iframe>
               <div className="text-[10px] text-gray-400 truncate mt-1">
                 <a
